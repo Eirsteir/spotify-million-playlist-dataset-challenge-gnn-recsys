975e5c680722d1d3fcfed282754f7ed30baa2ffb
Eirik Steira <steiraeirik@gmail.com>
2022-05-30 13:52:51 +0200

    Azure run configs

---
+++ run_pipeline.py
@@ -0,0 +1,24 @@
+# run-pytorch.py
+from azureml.core import Workspace
+from azureml.core import Experiment
+from azureml.core import Environment
+from azureml.core import ScriptRunConfig
+
+if __name__ == "__main__":
+    ws = Workspace.from_config()
+    experiment = Experiment(workspace=ws, name='day1-experiment-train')
+    config = ScriptRunConfig(source_directory='.',
+                             script='main.py',
+                             compute_target='cpu-cluster')
+
+    # set up pytorch environment
+    env = Environment.from_conda_specification(
+        name='gnn-env',
+        file_path='env.yml'
+    )
+    config.run_config.environment = env
+
+    run = experiment.submit(config)
+
+    aml_url = run.get_portal_url()
+    print(aml_url)
